6d80936260d890e7b7efe8723b68b3384af2203b
igor_alves <igornalves08@gmail.com>
2024-09-12 16:28:21 -0300

    colocando o eslint no projeto no qual vou fazer aqui algumas alteracoes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const server = fastify()
 
-server.listen({ 
-    port: 3454 
-}).then( () => {
+server
+  .listen({
+    port: 3454,
+  })
+  .then(() => {
     console.log('HTTP server running on port 3454 !!!')
-})
+  })
