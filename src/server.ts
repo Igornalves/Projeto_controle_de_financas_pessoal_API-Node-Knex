ee97f2142eb00bd883dd80db9b517913d5fb3920
igor_alves <igornalves08@gmail.com>
2024-09-12 18:10:16 -0300

    colocando um database no projeto para melhora o armazenamento dos dados

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import fastify from 'fastify'
+import { knex } from './database/database'
 
 const server = fastify()
+
+server.get('/testConnectionDatabase', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
+})
 
 server
   .listen({
